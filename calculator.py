f096b6ff8e961d5c7ac8dd6543b58eacb7f0b37a
baaba-midnight <baaba.amosah@gmail.com>
2024-03-07 22:08:26 +0000

    Implement Algorithm design Add placeholder functions in logicOperators.py and calculator.py main.py will run the main program and display the results

---
+++ calculator.py
@@ -0,0 +1,5 @@
+def sumOfProduct():
+    pass
+
+def productOfSum():
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from logicOperators import *
+from calculator import *
+
+# Ask user for choice: Sum of Product or Product of Sum
+# if sum of product call sumOfProduct
+# if product of sum call productOfSum function
+
+# depending on what the user input perform the respective logicOperators
