c07b21394732e79ee623af44d0a136d065474825
baaba-midnight <baaba.amosah@gmail.com>
2024-03-12 15:08:13 +0000

    Define SOP function Add variables and complements Define dictionary of values which will depend on the degree of the function

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
+var = ["r", "x", "y", "z"]
+var_complement = ["r̄" ,"x̄", "ȳ", "z̄"]
+
+valuesPerVariable = {"r": [], "x": [], "y": [], "z":[]}
+
+def SOP(DegreeOfFunction, table):
+    arrayOfPositions = [] # stores the positions with output as one
+    for row in arrayOfPositions:
+        currentString = ""
+        for col in range(DegreeOfFunction):
+            if table[row][col] == 0:
+                currentString += var_complement[col]
+            else:
+                currentString += var[col]
+        currentString += " + "
+
+def POS():
+    pass
+
+# construct table from a list of assigned values of 0s and 1s
+def table():
+    pass
+
+def main():
+    pass
+
 # Ask user to enter the degree of the function
-# Create the empty table with pre-filled values
+# make reference to var list to get variables
+
+# Use degree of the function to find the number of values per variable
+
+# Create the empty table with pre-filled values of variables
 # Ask user to input the values of the function in a string format with delimiters: " "
 # Display the values in the table
-# Ask the user if they want to do POS or SOP calculation
+# Ask the user if they want to do POS or SOP calculation	
 
-# POS: create an array that stores the positions of the values of 0
-# SOP: create an array that stores the positions of the values of 1
-
-# Use the index of the position to find the respective row in the table and return each value
-# for example: "x: 0" will return NOT(x) if the output is 1 SOP
-#              "x: 1" will return NOT(x) if output is 0 POS
-
-# 2 loops: Outer Loop (Index of 0 or 1) and Inner Loop (Degree of the function)
-
-# SOP 
-# arrayOfPositions = [2,4,8]
-# currentString = ""
-# finalString = ""
-# for x in range(len(arrayOfPositions)):
-#   for y in range(2):
-#       if table[x][y] == 0:
-#           currentString += "." + NOT(table[x][y]))
-#       else:
-#           currentString += " . " + table[x][y]  
-#   finalString += " + " + currentString
-
-# POS
-# arrayOfPositions = [2,4,8]
-# currentString = ""
-# finalString = ""
-# for x in range(len(arrayOfPositions)):
-#   for y in range(2):
-#       if table[x][y] == 0:
-#           currentString += "+" + NOT(table[x][y]))
-#       else:
-#           currentString += " + " + table[x][y]  
-#   finalString += " . " + currentString
+# ask for user input
