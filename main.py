d98c8676653d91ae8f8d5e573aedd5c474f3e03e
baaba-midnight <baaba.amosah@gmail.com>
2024-03-23 23:10:49 +0000

    final touches and tests

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ def SOP(DegreeOfFunction, stringOfValues, table):
     return currentString
 
 def POS(DegreeOfFunction, stringOfValues, table):
-    currentString = ""
     arrayOfPositions = [] # stores the positions with output as one
+    pos_expression = ""
 
     # get the positions of 1s in the table
     for index, x in enumerate(stringOfValues):
@@ -71,34 +71,15 @@ def POS(DegreeOfFunction, stringOfValues, table):
 
     # determine values for each index
     for row in arrayOfPositions:
+        currentString = ""
         for col in range(DegreeOfFunction):
             if table[row][col] == 1:
                 currentString += var_complement[col] +  " + "
             else:
                 currentString += var[col] + " + "
-        currentString = "(" + currentString[:-3] + ")"
+        pos_expression += "(" + currentString[:-3] + ")" # remove trailing "+"
 
-    return currentString
-    # arrayOfPositions =[]
-    # for index, i in enumerate(StringOfValues):
-    #     if i == "0":
-    #         arrayOfPositions.append(index)
-
-    # pos_expression = ""
-    # for row_index in arrayOfPositions:
-    #     current_string = ""
-    #     for col in range(DegreeOfFunction):
-    #         if table[row_index][col] == 1:
-    #             current_string += var_complement[col]
-    #         else:
-    #             current_string += var[col]
-    #             if col < DegreeOfFunction-1:
-    #                  current_string += " + "
-    #     pos_expression += "(" + current_string + ")" + " . "
-    
-    # pos_expression = pos_expression[:-3]
-
-    # return pos_expression
+    return pos_expression
 
 def displayTable(DegreeOfFunction, user_table):
     functionDef = "F("
@@ -112,14 +93,12 @@ def displayTable(DegreeOfFunction, user_table):
         else:
             functionDef += var[i] + ","
     
-    # print(len(functionDef))
     # values populate into display
-    space = 0
     for x in range(len(user_table)):
         for y in range(len(user_table[x])):
             if y == DegreeOfFunction-1:
                 space = 2
-            print(user_table[x][y], space * " " ,"\t", end='')
+            print(user_table[x][y], "\t", end='')
         print()
 
 
@@ -131,6 +110,7 @@ def main():
         # check if degree of function is greater than 4
         if DegreeOfFunction <= 4 and DegreeOfFunction > 0:
             break
+        print("Invalid Input. Try again. Enter a number between 0 and 5. Not Inclusive.")
 
         
     # Create the empty table with pre-filled values of variables
@@ -143,6 +123,7 @@ def main():
         user_Values = input("Enter your values in single line, for example, 0011\nYour Values: ")
         if len(user_Values) == 2**DegreeOfFunction:
             break
+        print(f"Invalid Input. Enter {2**DegreeOfFunction} values.")
 
     # append user values into array table
     for x in range(len(user_table)):
@@ -154,15 +135,16 @@ def main():
     print("SOP Expression:", SOP(DegreeOfFunction, user_Values, user_table))
     print("POS Expression:", POS(DegreeOfFunction, user_Values, user_table))
 
-
-print("Welcome to the sum of product and product of sum calculator. Enter")
-choice = int(input("1: Start \n2: End\nYour Choice:"))
 count = 0
-if choice == 1:
-    count += 1
-    main()
-else:
-    if count >= 1:
-        print("Thanks for using me!!")
+while True:
+    print("Welcome to the sum of product and product of sum calculator. Enter")
+    choice = input("1: Run Calculator \n2: Enter <space>\nYour Choice: ")
+    if choice == "1":
+        count += 1
+        main()
     else:
-        print("Bye!!")
+        if count >= 1:
+            print("Thanks for using me!!")
+        else:
+            print("Bye!!")
+        break
